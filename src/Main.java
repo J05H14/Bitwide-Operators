5fc4ad1e97c4c982dd46bfb39953d2ebb5ea6f8d
J05H14 <j05h14@yahoo.com>
2017-05-02 14:26:12 -0700

    hide message done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,45 @@
 
 
 	
-import PPMImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		PPMImage ppm = new PPMImage("");
+		File PPMImage = null;		
+		JFileChooser fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("PPM Files", "ppm");
+		fc.setFileFilter(filter);
+		int retVal = fc.showOpenDialog(null);
 		
+		if(retVal == JFileChooser.APPROVE_OPTION){
+			PPMImage = fc.getSelectedFile();
+		}	
+		
+		PPMSub ppm = new PPMSub(PPMImage);
+		ppm.hideMessage("hi");
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Image image = SwingFXUtils.toFXImage(ImageIO.read(PPMImage), null);
+			ImageView iv = new ImageView(image);
+			
+			
+			root.getChildren().add(iv);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/PPMSub.java
+++ src/PPMSub.java
@@ -6,5 +6,48 @@ public class PPMSub extends PPMImage {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
+	public void hideMessage(String message){
+		for(int i = 1; i < message.length(); i++){
+			int colorCount = 0;
+			char letter = message.charAt(i);
+			
+			for(int bit = 0; bit < 9; bit++){
+				char mask = (char) (1 << (bit - 1));
+
+				if((letter & mask) == 0){
+					mask = (char) ~1;
+					
+					getPixelData()[colorCount] = (char) (getPixelData()[colorCount] & mask);
+					
+				}
+				else{
+					mask = 1;
+					
+					getPixelData()[colorCount] = (char) (getPixelData()[colorCount] & mask);
+				}
+				colorCount++;
+			}
+		}
+		
+		writeImage("PPM_with_message.ppm");
+	}
+	public String recoverMesssage(){
+		String message = "";
+		
+//		for(int ){
+//			
+//		}
+		
+		return message;
+	}
+	public void sepia(){
+
+	}
+	public void grayscale(){
+
+	}
+	public void negative(){
+
+	}
 
 }
